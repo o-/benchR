72b1d5a2c363da5de8a4e17ee4a2c6f8df7a2d71
helenaK <helena.kotthaus@tu-dortmund.de>
2013-11-11 16:06:11 +0100

    Update rda_as_lda.R

--- MachineLearningAlg/main_functions/rda_as_lda.R
+++ MachineLearningAlg/main_functions/rda_as_lda.R
@@ -1,9 +1,20 @@
 # ------------------------------------------------------------------
-# Contributed by Michel Lang, TU Dortmund
-# ------------------------------------------------------------------
-# Linear discriminant analysis using the klaR package with default parameters
+# This material is distributed under the GNU General Public License
+# Version 2. You may review the terms of this license at
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
+# Copyright (c) 2012-2013, Michel Lang, Helena Kotthaus,
+# TU Dortmund University
+#
+# All rights reserved.
+#
+# Linear discriminant analysis using the klaR package with 
+# default parameters
+#
 # USEAGE: Rscript [scriptfile] [problem-number] [number of replications]
 # Output: Misclassification rate
+# ------------------------------------------------------------------
+
 library(klaR)
 type <- "classification"
 
